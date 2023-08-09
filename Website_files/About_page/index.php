5e4a446dc3a4ba1109541efc93ab2aec1eeea934
dosterevan <105185770+dosterevan@users.noreply.github.com>
2023-08-09 16:08:34 -0700

    Updating Branch

--- Website_files/About_page/index.php
+++ Website_files/About_page/index.php
@@ -15,8 +15,9 @@
   <header>
     <nav>
       <ul>
-        <li><a href="../../Home_Page/index.php">Home</a></li>
-        <li><a href="https://www.fema.gov/about/contact">Contact</a></li>
+        <!-- Route the website correctly now-->
+        <a href="../../index.php">Home</a>
+        <a <span style="padding-left: 55px;" href="https://www.fema.gov/about/contact">Contact </span></a>
       </ul>
     </nav>
   </header>
@@ -25,12 +26,13 @@
     <div class="content">
       <p><center><span style="padding-left: 0px;">The FEMAp project aims to create a SaaS database for emergency preparedness, 
         providing users with customized checklists based on FEMA's recommended emergency lists. 
-        The system will generate printable/interactive PDFs with the user's selected emergencies and supply lists. </span></center></p>
+        The system will generate printable/interactive PDFs with the user's selected emergencies and supply lists. We are not affiliated 
+        with FEMA.</span></center></p>
     </div>
   </section>
 
   <footer>
-    <p><span style="padding-left: 25px;"></span>&copy; 2023 FEMAp, we are not affiliated with FEMA.</span></p>
+    <p><span style="padding-left: 75px;">2023 FEMAp, we are not affiliated with FEMA.</span></p>
   </footer>
 
   </html>
