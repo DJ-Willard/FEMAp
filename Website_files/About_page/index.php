1255ad941e6aca7cc0a21aed88a668da51cfcb68
dosterevan <105185770+dosterevan@users.noreply.github.com>
2023-08-11 18:38:35 -0700

    Added link to home and about page

--- Website_files/About_page/index.php
+++ Website_files/About_page/index.php
@@ -18,6 +18,7 @@
         <!-- Route the website correctly now-->
         <a href="../../index.php">Home</a>
         <a <span style="padding-left: 55px;" href="https://www.fema.gov/about/contact">Contact </span></a>
+        <a <span style="padding-left: 75px;" href="https://www.mayoclinic.org/first-aid/first-aid-kits/basics/art-20056673">First Aid Kit Supplies</span></a>
       </ul>
     </nav>
   </header>
